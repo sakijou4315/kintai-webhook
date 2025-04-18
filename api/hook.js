585c9d82f312d918049486725709f5b985c32b61
sakijou4315 <136298629+sakijou4315@users.noreply.github.com>
2025-04-18 18:54:52 +0900

    Create hook.js

---
+++ api/hook.js
@@ -0,0 +1,8 @@
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    console.log('✅ Webhook Received:', req.body);
+    res.status(200).json({ message: 'Webhook received successfully!' });
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+}
